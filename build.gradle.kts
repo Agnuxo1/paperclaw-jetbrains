c7a1d5c5f2368371d0d16f0ba18445af507fc040
Francisco Angulo de Lafuente <166046035+Agnuxo1@users.noreply.github.com>
2026-04-17 15:43:10 +0200

    Add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.22"
+    id("org.jetbrains.intellij") version "1.17.3"
+}
+
+group = "com.agnuxo1.paperclaw"
+version = "1.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+intellij {
+    version.set("2023.3")
+    type.set("IC")
+    plugins.set(listOf<String>())
+}
+
+tasks {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("233")
+        untilBuild.set("243.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
